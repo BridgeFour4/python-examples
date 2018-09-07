3055a5055dabac872046f772cb699177f2c93d25
Nathan Broadbent <nathan.broadbent@tooeleschools.org>
2018-09-07 13:25:51 -0600

    Chapter one examples

---
+++ example variables.py
@@ -0,0 +1,7 @@
+mes1="this is some \"data\""
+
+print(mes1)
+
+x=5
+if x==5:
+    print(x)
---
+++ mad lib.py
@@ -0,0 +1,48 @@
+##print("hi class")
+##
+##first_name="Nathan"
+##print(first_name)
+##
+##fact="i love video games"
+##print("my name is ",first_name, " and ",fact)
+##message = first_name+fact
+##print(message)
+##
+##num1 =1
+##num2= 2
+##answer = num1 + num2 -num1*num1
+##print(answer)
+
+##word1=input("adjective")
+##word2=input("noun (person)")
+##word3=input("noun (place)")
+##word4=input("verb")
+##word5=input("noun ")
+##word6=input("noun (thing)")
+##word7=input("verb")
+##word8=input("verb")
+##
+##print("the ",
+##      word1,
+##      word2,
+##      "went for a walk around a " ,
+##      word3, "while ",
+##      word4,"into a ", word5,
+##      "with its pet " ,word6,
+##      "Now as the ",word2,
+##      " was wandering around they found another ",
+##      word6," and they weren't sure what to do so they ", word7,
+##      " them away and never ", word8, " them again")
+
+score1=input("score")
+score2=input("score")
+score2=int(score2)
+
+score3=input("score")
+score4=input("score")
+score5=input("score")
+
+sum1=int(score1)+score2+int(score3)+int(score3)+int(score4)+int(score5)
+avg= sum1/5
+print(avg)
+input()
