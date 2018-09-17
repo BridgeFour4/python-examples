fc56a44d0c1b52e23d8073665b5cb00a1834f7a6
Nathan Broadbent <nathan.broadbent@tooeleschools.org>
2018-09-17 14:00:54 -0600

    added some new functions

--- Name checker.py
+++ Name checker.py
@@ -1,35 +1,70 @@
 #Nathan Broadbent
 #9/18
-
+import math
 # get a users name
-##def get_name():
-##    
-###step 1 ask for a name
-##    name= input("What is your name")
-### step 2 display Name to User
-##    print("the name you entered was", name)
-###step 3  verify the name
-##    verify= input("is this correct yes or no")
-##
-##print("this is our function")    
-##get_name()
-
-
-
-#calculate the area of a circle
-#radius squared*pi
-
-
-#step 1 get the radius
-#step 2 calculate the area
-#step three display area
-
-def circle_area():
-    pi=3.141592653
-    radius= input("what is the radius ")
-    radius= float(radius)
-
-    area= radius*radius*pi
-    print("the area of your circle is ", area)
+def get_name(name):
+    input_name= name
+    input_name= input_name.lower()
+    input_name=input_name.title()
     
-circle_area();
+    
+    print("the name you entered was", input_name)
+#step 3  verify the name
+    verify= input("is this correct yes or no")
+
+print("this is our function")
+name= input("What is your name")
+get_name(name)
+
+
+##
+###calculate the area of a circle
+###radius squared*pi
+##
+##
+###step 1 get the radius
+###step 2 calculate the area
+###step three display area
+##
+##def circle_area():
+##    pi=3.141592653
+##    radius= input("what is the radius ")
+##    radius= float(radius)
+##
+##    area= radius*radius*pi
+##    print("the area of your circle is ", area)
+##    
+##circle_area();
+
+#pythagorean theorum
+
+##def pythag_theorum(ap=1,bp=1):
+##    a= float(ap)
+##    b= float(bp)
+##    c= a*a + b*b
+##    c=math.sqrt(c)
+##
+##    print("the third side is",c)
+##
+##    
+##ai=input ("the first side is what ")
+##bi=input ("the second side is what ")
+##pythag_theorum(ai,bi)
+
+
+##def add_numbers(x,y):
+##    num1=x
+##    num2=y
+##    num3= float(num1)+float(num2)
+##    return num3
+##
+##x= input("enter a number")
+##y=input("enter a number")    
+##num4 = add_numbers(x,y)
+##print(num4)
+
+
+
+
+
+    
